5af18db124e50d5b631b31a8ec85980865b3a9a7
qianlifeng <qianlf2008@163.com>
2018-01-18 15:01:24 +0800

    增加GPU信息

--- src/main/java/monitorx/monitorxethminer/ReportSchedule.java
+++ src/main/java/monitorx/monitorxethminer/ReportSchedule.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import monitorx.monitorxethminer.statusReport.Metric;
 import monitorx.monitorxethminer.statusReport.NodeStatus;
 import monitorx.monitorxethminer.statusReport.NodeStatusUpload;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +13,13 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author qianlifeng
@@ -27,6 +32,9 @@ public class ReportSchedule {
     @Autowired
     EthMinerService ethMinerService;
 
+    Pattern GPUTemperaturePattern = Pattern.compile("GPU Temperature:(.*)C");
+    Pattern GPULoadPattern = Pattern.compile("GPU Load:(.*) %");
+
     private Date lastUploadDate;
 
     @Value("${code}")
@@ -35,21 +43,17 @@ public class ReportSchedule {
     @Value("${url}")
     private String url;
 
-    @Scheduled(fixedDelay = 1000)
-    public void doNothing() {
-        Date lastTailDate = ethMinerService.getLastTailDate();
-        if (lastTailDate != null && (lastUploadDate == null || lastUploadDate.compareTo(lastTailDate) != 0)) {
-            if (ethMinerService.getLastTailMh() != null) {
-                lastUploadDate = lastTailDate;
+    @Value("${gpuFolder}")
+    private String gpuFolder;
 
-                upload(lastTailDate, ethMinerService.getLastTailMh());
-            }
-        }
+    @Scheduled(fixedDelay = 3000)
+    public void report() {
+        upload();
     }
 
-    private void upload(Date lastTailDate, Integer lastTailMh) {
+    private void upload() {
         try {
-            logger.info("reporting to monitorx: {} {}", lastTailMh, lastTailDate);
+            logger.info("reporting to monitorx");
             NodeStatusUpload statusUpload = new NodeStatusUpload();
             NodeStatus status = new NodeStatus();
             statusUpload.setNodeCode(code);
@@ -62,12 +66,77 @@ public class ReportSchedule {
             Metric currentHandlingOrders = new Metric();
             currentHandlingOrders.setTitle("实时算力");
             currentHandlingOrders.setType("number");
-            currentHandlingOrders.setValue(lastTailMh.toString());
             metrics.add(currentHandlingOrders);
+            Date lastTailDate = ethMinerService.getLastTailDate();
+            Integer lastTailMh = ethMinerService.getLastTailMh();
+            if (lastTailDate != null && (lastUploadDate == null || lastUploadDate.compareTo(lastTailDate) != 0)) {
+                if (ethMinerService.getLastTailMh() != null) {
+                    lastUploadDate = lastTailDate;
+                    currentHandlingOrders.setValue(lastTailMh.toString());
+                }
+            } else {
+                currentHandlingOrders.setValue("0");
+            }
+
+            Metric gpuMetric = new Metric();
+            List<Map<String, String>> gpuInfo = getGPUInfo();
+            gpuMetric.setTitle("GPU信息");
+            gpuMetric.setType("text");
+            StringBuilder sb = new StringBuilder();
+            sb.append("<table class='table table-bordered'>");
+            sb.append("     <tr>");
+            sb.append("         <th width='60'>序号</th>");
+            sb.append("         <th>温度</th>");
+            sb.append("         <th>负载</th>");
+            sb.append("     </tr>");
+            for (Map<String, String> gpu : gpuInfo) {
+                sb.append("     <tr>");
+                sb.append("         <td>" + gpu.get("index") + "</td>");
+                sb.append("         <td>" + gpu.get("temperature") + "</td>");
+                sb.append("         <td>" + gpu.get("load") + "</td>");
+                sb.append("     </tr>");
+            }
+            sb.append("</table>");
+
+            gpuMetric.setValue(sb.toString());
+            gpuMetric.setContext(JSON.toJSONString(gpuInfo));
+            metrics.add(gpuMetric);
 
             HTTPUtil.sendBodyPost(url, JSON.toJSONString(statusUpload));
         } catch (IOException e) {
             logger.error("upload to monitorx error: " + e.getMessage(), e);
         }
+    }
+
+    private List<Map<String, String>> getGPUInfo() {
+        List<Map<String, String>> info = new ArrayList<>();
+        if (StringUtils.isNotEmpty(gpuFolder)) {
+            for (int i = 1; i < 7; i++) {
+                Path path = Paths.get(gpuFolder, i + "", "amdgpu_pm_info");
+                try {
+                    String content = new String(Files.readAllBytes(path));
+                    Matcher temperatureMatcher = GPUTemperaturePattern.matcher(content);
+                    Matcher loadMatcher = GPULoadPattern.matcher(content);
+                    if (temperatureMatcher.find() && loadMatcher.find()) {
+                        Map<String, String> infoMap = new HashMap<>();
+                        String temperature = temperatureMatcher.group(1).trim();
+                        String load = loadMatcher.group(1).trim();
+
+                        infoMap.put("index", i + "");
+                        infoMap.put("temperature", temperature + " C");
+                        infoMap.put("load", load + "%");
+                        info.add(infoMap);
+                    } else {
+                        logger.info("didn't find");
+                    }
+                } catch (NoSuchFileException e) {
+                    break;
+                } catch (IOException e) {
+                    logger.error("read gpu info failed, path={}", path.toString(), e);
+                }
+            }
+        }
+
+        return info;
     }
 }
